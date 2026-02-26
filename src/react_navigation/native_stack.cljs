d14de9a27a87772178adfde355cb0e4894df9823
Ulises M <ulises.ssb@hotmail.com>
2026-02-25 19:37:16 -0600

    Add native-stack wrapper

--- src/react_navigation/native_stack.cljs
+++ src/react_navigation/native_stack.cljs
@@ -1,9 +1,29 @@
 (ns react-navigation.native-stack
   (:require
-    ["@react-navigation/native-stack" :refer [createNativeStackNavigator]]
-    [reagent.core :as r]))
+   ["@react-navigation/native-stack" :refer [createNativeStackNavigator]]
+   [react-navigation.navigator.impl :as impl]
+   [reagent-extended-compiler.utils.transforms :as transforms]
+   [reagent.core :as r]))
 
-(defn create-native-stack-navigator []
-  (let [stack (createNativeStackNavigator)]
-    {:navigator (r/adapt-react-class (.-Navigator stack))
-     :screen    (r/adapt-react-class (.-Screen stack))}))
+(defn- reactify-screens-map [m]
+  (update m :screens impl/->js-screens-data))
+
+(defn- reactify-groups-map [config]
+  (update config :groups
+          (fn [groups]
+            (update-vals groups
+                         (fn [group]
+                           (cond-> group
+                             (:screens group) reactify-screens-map))))))
+
+(defn create-native-stack-navigator
+  ([]
+   (let [stack (createNativeStackNavigator)]
+     {:navigator (r/adapt-react-class (.-Navigator stack))
+      :screen    (r/adapt-react-class (.-Screen stack))}))
+  ([config]
+   (cond-> config
+     (:screens config) reactify-screens-map
+     (:groups config)  reactify-groups-map
+     :always           transforms/->js-prop-obj
+     :always           (createNativeStackNavigator))))
